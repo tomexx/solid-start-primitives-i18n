c13dba8c2aada73c85c07fc227d22f4b239762cf
Tomas Listiak <tomexx@tomexx.net>
2023-02-10 01:09:45 +0100

    this doesn't work

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   const [t, { add, locale, dict }] = useI18n();
   return (
     <main>
-      <SeoTitle />
+      <Title>
+        <SeoTitle />
+      </Title>
       <div>
         <button onClick={() => locale("fr")}>fr</button>
         <button onClick={() => locale("en")}>en</button>
