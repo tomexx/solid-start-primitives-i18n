0018df8f99e7319acfe4027272ced35347aa0c80
Tomas Listiak <tomexx@tomexx.net>
2023-02-11 23:27:28 +0100

    example -> this works

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,20 @@ import { Title } from "solid-start";
 import { seoTitle } from "~/util/SeoTitle";
 import Counter from "~/components/Counter";
 import { I18nContext, createI18nContext, useI18n } from "@solid-primitives/i18n";
+import { createEffect, createSignal } from "solid-js";
 
 export default function Home() {
   const [t, { add, locale, dict }] = useI18n();
+  const [title, setTitle] = createSignal(seoTitle())
+
+  createEffect(() => {
+    setTitle(seoTitle())
+  });
+  
   return (
     <main>
       <Title>
-        {seoTitle()}
+        {title()}
       </Title>
       <div>
         <button onClick={() => locale("fr")}>fr</button>
